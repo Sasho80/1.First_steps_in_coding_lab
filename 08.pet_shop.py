2c26c3f430392b63fe6b32f229f0c95667d961d5
Aleksandar <Sasho80@gbg.bg>
2024-04-29 22:03:04 +0300

    Add files via upload

---
+++ 08.pet_shop.py
@@ -0,0 +1,7 @@
+number_package_dog=float(input())
+number_package_cat=float(input())
+
+COST_ONE_PACKAGE_DOG_FOOD=2.50
+COST_ONE_PACKAGE_CAT_FOOD=4
+
+print(f"{number_package_dog*COST_ONE_PACKAGE_DOG_FOOD+number_package_cat*COST_ONE_PACKAGE_CAT_FOOD } lv.")
